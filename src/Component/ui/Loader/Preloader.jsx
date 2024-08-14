1279eda3159bc297a98cfce2fcd520782d7e4b3e
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-08-15 04:32:34 +0600

    add loader

---
+++ src/Component/ui/Loader/Preloader.jsx
@@ -0,0 +1,13 @@
+// Preloader.js
+import React from "react";
+import "./Preloader.css"; // Import custom CSS for the preloader
+
+const Preloader = () => {
+  return (
+    <div className="preloader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Preloader;
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,7 +3,23 @@ import Navbar from "./Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import "./Main.css";
+import  { useState, useEffect } from "react";
+import Preloader from "../Component/ui/Loader/Preloader";
 const Main = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate an API call or other loading processes
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Adjust the timeout as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Preloader />; // Show the preloader if loading
+  }
   return (
     <div className="bg">
       <Navbar></Navbar>
